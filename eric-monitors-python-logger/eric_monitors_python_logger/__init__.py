645274c55ff5de915c073c9a43bcd0cf35d5770c
luca <laxertu@gmail.com>
2026-01-06 08:45:38 +0100

    0.1.0pre

---
+++ eric-monitors-python-logger/eric_monitors_python_logger/__init__.py
@@ -0,0 +1,25 @@
+from logging import Handler
+
+from eric_sse.entities import AbstractChannel
+from eric_sse.message import MessageContract, Message
+
+class LoggingChannel(AbstractChannel):
+
+    def adapt(self, msg: MessageContract) -> MessageContract:
+        return msg
+
+class EricHandler(Handler):
+
+    def __init__(self, level=0, stream_delay_seconds=0):
+        super().__init__(level)
+        self.__channel = LoggingChannel(stream_delay_seconds=stream_delay_seconds)
+
+    @property
+    def channel(self) -> AbstractChannel:
+        return self.__channel
+
+    def handle(self, record):
+        self.__channel.broadcast(Message(msg_type=record.levelname, msg_payload=self.format(record)))
+
+    def emit(self, record):
+        pass
