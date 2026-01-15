a9e398381537663083f172adf49fac4d239cd68c
luca <laxertu@gmail.com>
2026-01-15 18:27:39 +0100

    0.0.1

--- eric-monitors-watchdog/examples/example_watchdog.py
+++ eric-monitors-watchdog/examples/example_watchdog.py
@@ -2,7 +2,7 @@ import asyncio
 import threading
 import sys
 from eric_monitors_watchdog import WatchDogChannel, WatchDogEventHandler
-from watchdog.events import DirModifiedEvent, FileModifiedEvent
+from watchdog.events import FileModifiedEvent
 
 try:
     path = sys.argv[1]
@@ -10,24 +10,32 @@ except IndexError:
     print("Usage: python example_watchdog.py <path to monitor>")
     sys.exit(1)
 
+
 class MyHandler(WatchDogEventHandler):
     """
     Extend base Handler to access to notify method, then define events you are interested to
     """
-    def on_modified(self, event: DirModifiedEvent | FileModifiedEvent) -> None:
+    def on_modified(self, event: FileModifiedEvent) -> None:
         self.notify(event)
 
-c = WatchDogChannel(event_handler_class=MyHandler, directory_to_monitor=path)
-l = c.add_listener()
-l.start()
 
 async def main():
+    c = WatchDogChannel(event_handler_class=MyHandler, directory_to_monitor=path)
+    l = c.add_listener()
+    l.start()
+
     t = threading.Thread(target=c.start)
     t.daemon = True
     t.start()
 
-    async for m in c.message_stream(listener=l):
-        print(f"[{m.type}] {m.payload}")
+    print(f"Create of modify some file in {path}")
+
+    try:
+        async for m in c.message_stream(listener=l):
+            print(f"[{m.type}] {m.payload}")
+    except asyncio.CancelledError:
+        print("\nExiting")
+        exit(0)
 
 if __name__ == '__main__':
 
